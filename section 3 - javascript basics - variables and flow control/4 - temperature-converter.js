13ff6bf80cb60bf57b4a1a499c785940e01f0914
Rendi K <rendi.krinaldi@gmail.com>
2021-05-06 00:44:16 +0700

    temperature converter

---
+++ section 3 - javascript basics - variables and flow control/4 - temperature-converter.js
@@ -0,0 +1,12 @@
+let fahrenheit = 50
+console.log('fahrenheit: ', fahrenheit)
+
+// calculate the celcius and store in celcius variable
+let celcius = (fahrenheit - 32) * 5 / 9
+// print the value
+console.log('converted to celcius: ', celcius)
+
+// calculate the kelvin and store in kelvin variable
+let kelvin = (fahrenheit + 459.67) * 5 / 9
+// print the value
+console.log('converted to kelvin: ', kelvin)
