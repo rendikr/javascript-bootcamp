f245610725409cc9a528be5e8578e5bd5c941779
Rendi K <rendi.krinaldi@gmail.com>
2021-05-06 01:06:46 +0700

    if statement

---
+++ section 3 - javascript basics - variables and flow control/6 - if-statements.js
@@ -0,0 +1,20 @@
+let temp = 180
+
+if (temp <= 32) {
+  console.log('it is freezing outside!')
+}
+
+if (temp >= 110) {
+  console.log('it is way too hot outside!')
+}
+
+// Challenge
+let age = 5
+
+if (age <= 7) {
+  console.log('you get a child price!')
+}
+
+if (age >= 65) {
+  console.log('you get a senior discount!')
+}
