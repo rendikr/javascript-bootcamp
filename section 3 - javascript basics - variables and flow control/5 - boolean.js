6222ffd65e9abbe7e464b839395b01e14357ae06
Rendi K <rendi.krinaldi@gmail.com>
2021-05-06 00:58:42 +0700

    boolean and comparison operators

---
+++ section 3 - javascript basics - variables and flow control/5 - boolean.js
@@ -0,0 +1,33 @@
+let temp = 31
+
+// ===  : equality operator
+let isFreezing = temp === 31
+console.log(isFreezing)
+
+// !==  : no equal operator
+isFreezing = temp !== 32
+console.log(isFreezing)
+
+// <    : less than
+isFreezing = temp < 32
+console.log(isFreezing)
+
+// <=   : less than or equal
+isFreezing = temp <= 31
+console.log(isFreezing)
+
+// >    : greater than
+isFreezing = temp > 31
+console.log(isFreezing)
+
+// >=   : greater than or equal
+isFreezing = temp >= 31
+console.log(isFreezing)
+
+// Challenge
+let age = 31
+let isChild = age <= 7
+let isSenior = age >= 65
+
+console.log('customer is child: ', isChild)
+console.log('customer is senior: ', isSenior)
