39c4a0d14bbf2508fcc9b270d94684ad21b5262d
Rendi K <rendi.krinaldi@gmail.com>
2021-05-06 00:39:44 +0700

    more on variables

---
+++ section 3 - javascript basics - variables and flow control/3 - variables.js
@@ -0,0 +1,15 @@
+// 1. you can't define a variable more than once
+let petName = 'Hal'
+// let petName = 'Spot'
+petName = 'Spot'
+
+console.log(petName)
+
+// 2. there are rules related to variable names
+// let 3 = 2
+let test_ = 2
+let result = test_ + 4
+
+// 3. variable names cannot be reserved keywords
+// let let = 12
+
