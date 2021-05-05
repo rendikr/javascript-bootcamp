6a2d1e90521c3178cfa23b44abb3f9d7f9ebcce5
Rendi K <rendi.krinaldi@gmail.com>
2021-05-06 00:31:49 +0700

    numbers

---
+++ section 3 - javascript basics - variables and flow control/2 - numbers.js
@@ -0,0 +1,16 @@
+let age = 31
+let dogYears = (age + 1) / 7
+
+console.log(dogYears)
+
+// challenge
+
+// studentScore
+let studentScore = 18
+// maxScore
+let maxScore = 20
+
+// calculate the percentage
+let percentage = (studentScore * 100) / maxScore
+
+console.log(percentage)
