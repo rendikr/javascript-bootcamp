bb3535ce442c2b9ae08e34653b4e9925f173ac12
rendikr <rendi.krinaldi@gmail.com>
2021-05-11 15:49:14 +0700

    array basics

---
+++ section 6 - javascript arrays/todo.js
@@ -0,0 +1,6 @@
+// create an array with five todos
+const todos = ['Todo A', 'Todo B', 'Todo C', 'Todo D', 'Todo E']
+
+console.log(`You have ${todos.length} todos`)
+console.log(`Todo: ${todos[0]}`)
+console.log(`Todo: ${todos[todos.length - 2]}`)
