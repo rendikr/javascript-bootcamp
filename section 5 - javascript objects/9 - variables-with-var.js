65537d33999fd967dedcb902d7b531bb67be4982
rendikr <rendi.krinaldi@gmail.com>
2021-05-11 15:32:11 +0700

    variables with var

---
+++ section 5 - javascript objects/9 - variables-with-var.js
@@ -0,0 +1,21 @@
+var firstName = 'Andrew'
+firstName = 'Mike'
+
+console.log(firstName)
+
+var firstName = 'Jen'
+
+console.log(firstName)
+
+if (10 === 10) {
+  var firstName = 'Dave'
+}
+
+console.log(firstName)
+
+const setName = function () {
+  var firstName = 'Dune'
+}
+
+setName()
+console.log(firstName)
