c9d69e0de99cfec61d659a0542731d148a6efa6c
rendikr <rendi.krinaldi@gmail.com>
2021-05-11 15:20:11 +0700

    constant variables

---
+++ section 5 - javascript objects/8 - constants.js
@@ -0,0 +1,21 @@
+let isRaining = true
+
+isRaining = false
+
+console.log(isRaining)
+
+const isHoliday = false
+
+console.log(true)
+
+const person = {
+  age: 27,
+}
+
+// this will break the code
+// person = {}
+
+// this is valid
+person.age = 28
+
+console.log(person)
