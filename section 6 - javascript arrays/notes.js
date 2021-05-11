1ee924b24f6fafbd8276cda4d2d39f4817d4f24b
rendikr <rendi.krinaldi@gmail.com>
2021-05-11 17:02:34 +0700

    manipulating array with methods

--- section 6 - javascript arrays/notes.js
+++ section 6 - javascript arrays/notes.js
@@ -1,6 +1,24 @@
 const notes = ['Note 1', 'Note 2', 'Note 3']
 
+// remove the first item from an array
+notes.shift()
+
+// remove the last item from an array
+notes.pop()
+
+// insert a new item as the first item of an array
+notes.unshift('My first note')
+
+// insert a new item into the end of an array
+notes.push('My new note')
+
+console.log(notes)
+
+// remove an item from an array
+notes.splice(1, 1)
+
+// add an item from an array
+notes.splice(1, 0, 'My new second note')
+
 console.log(notes)
 console.log(notes.length)
-
-console.log(notes[0])
--- section 6 - javascript arrays/todo.js
+++ section 6 - javascript arrays/todo.js
@@ -1,6 +1,16 @@
 // create an array with five todos
 const todos = ['Todo A', 'Todo B', 'Todo C', 'Todo D', 'Todo E']
 
+// delete the 3rd item
+todos.splice(2, 1)
+
+// add a new item onto the end
+todos.push('Todo F')
+
+// remove the first item
+todos.shift()
+
 console.log(`You have ${todos.length} todos`)
+console.log(`${todos}`)
 console.log(`Todo: ${todos[0]}`)
 console.log(`Todo: ${todos[todos.length - 2]}`)
