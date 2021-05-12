53039edd10275088098e3f902b07dd974b9027df
rendikr <rendi.krinaldi@gmail.com>
2021-05-12 15:12:10 +0700

    DOM Manipulation

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,2 +1,16 @@
 // DOM - Document Object Model
-console.log('this is from a different file')
+
+// Query and remove
+// note: if there are two <p> element, only the first one will be affected
+const p = document.querySelector('p')
+console.log(p)
+p.remove()
+
+// Query all and remove
+const ps = document.querySelectorAll('p')
+
+ps.forEach(function (p) {
+  p.textContent = 'Changing the value from javascript'
+  console.log(p.textContent)
+  p.remove()
+})
