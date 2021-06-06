6c7069a0bb2d20a7ea887974fb595ce5c6c5000f
rendikr <rendi.krinaldi@gmail.com>
2021-06-06 18:59:37 +0700

    debugging applications

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,35 +1,35 @@
 // DOM Manipulation
 
-let todoList = getSavedTodoList()
+let todoList = getSavedTodoList();
 
 const filters = {
   searchText: '',
   hideCompleted: false,
-}
+};
 
-renderTodoList(todoList, filters)
+renderTodoList(todoList, filters);
 
 document.querySelector('#search-text').addEventListener('input', function (e) {
-  filters.searchText = e.target.value
-  renderTodoList(todoList, filters)
-})
+  filters.searchText = e.target.value;
+  renderTodoList(todoList, filters);
+});
 
 document.querySelector('#new-todo').addEventListener('submit', function (e) {
-  e.preventDefault()
-  const newTodo = e.target.elements.newTodo.value
+  e.preventDefault();
+  const newTodo = e.target.elements.newTodo.value;
   todoList.push({
     text: newTodo,
     completed: false,
-  })
+  });
 
-  e.target.elements.newTodo.value = ''
-  saveTodoList(todoList)
-  renderTodoList(todoList, filters)
-})
+  e.target.elements.newTodo.value = '';
+  saveTodoList(todoList);
+  renderTodoList(todoList, filters);
+});
 
 document
   .querySelector('#hide-completed')
   .addEventListener('change', function (e) {
-    filters.hideCompleted = e.target.checked
-    renderTodoList(todoList, filters)
-  })
+    filters.hideCompleted = e.target.checked;
+    renderTodoList(todoList, filters);
+  });
--- todo-app/todo-functions.js
+++ todo-app/todo-functions.js
@@ -1,55 +1,62 @@
 const getSavedTodoList = function () {
-  const todoJSON = localStorage.getItem('todoList')
+  const todoJSON = localStorage.getItem('todoList');
 
   if (todoJSON !== null) {
-    return JSON.parse(todoJSON)
+    return JSON.parse(todoJSON);
   }
 
-  return []
-}
+  return [];
+};
 
 const saveTodoList = function (todoList) {
-  localStorage.setItem('todoList', JSON.stringify(todoList))
-}
-
-const generateTodoDOM = function (todo) {
-  const todoEl = document.createElement('p')
-
-  if (todo.text.length > 0) {
-    todoEl.textContent = todo.text
-  } else {
-    todoEl.textContent = 'Unnamed todo'
-  }
-
-  return todoEl
-}
-
-const generateTodoSummaryDOM = function (filteredTodoList) {
-  const incompleteTodoList = filteredTodoList.filter(function (todo) {
-    return !todo.completed
-  })
-
-  const summary = document.createElement('h2')
-  summary.textContent = `You have ${incompleteTodoList.length} todo left`
-}
+  localStorage.setItem('todoList', JSON.stringify(todoList));
+};
 
 const renderTodoList = function (todoList, filters) {
   let filteredTodoList = todoList.filter(function (todo) {
     const searchTextMatch = todo.text
       .toLowerCase()
-      .includes(filters.searchText.toLowerCase())
-    const hideCompletedMatch = !filters.hideCompleted || !todo.completed
+      .includes(filters.searchText.toLowerCase());
+    const hideCompletedMatch = !filters.hideCompleted || !todo.completed;
+    debugger
+    return searchTextMatch && hideCompletedMatch;
+  });
 
-    return searchTextMatch && hideCompletedMatch
-  })
+  const incompleteTodoList = filteredTodoList.filter(function (todo) {
+    return !todo.completed;
+  });
 
-  document.querySelector('#todo-list').innerHTML = ''
+  document.querySelector('#todo-list').innerHTML = '';
 
-  const summaryEl = generateTodoSummaryDOM(filteredTodoList)
-  document.querySelector('#todo-list').appendChild(summaryEl)
+  const incompleteEl = generateTodoSummaryDOM(incompleteTodoList);
+  document.querySelector('#todo-list').appendChild(incompleteEl);
+
+  // const summaryEl = generateTodoSummaryDOM(filteredTodoList);
+  // document.querySelector('#todo-list').appendChild(summaryEl);
 
   filteredTodoList.forEach(function (todo) {
-    const todoEl = generateTodoDOM(todo)
-    document.querySelector('#todo-list').appendChild(todoEl)
-  })
-}
+    const todoEl = generateTodoDOM(todo);
+    document.querySelector('#todo-list').appendChild(todoEl);
+  });
+};
+
+const generateTodoDOM = function (todo) {
+  const todoEl = document.createElement('p');
+
+  if (todo.text.length > 0) {
+    todoEl.textContent = todo.text;
+  } else {
+    todoEl.textContent = 'Unnamed todo';
+  }
+
+  return todoEl;
+};
+
+const generateTodoSummaryDOM = function (filteredTodoList) {
+  const incompleteTodoList = filteredTodoList.filter(function (todo) {
+    return !todo.completed;
+  });
+
+  const summary = document.createElement('h2');
+  summary.textContent = `You have ${incompleteTodoList.length} todo left`;
+};
